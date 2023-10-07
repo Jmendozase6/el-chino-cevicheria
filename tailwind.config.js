4038ca88802b2a7b48c6b5000179dfc0689c97b1
Jhair Mendoza <Felinecode.dev@gmail.com>
2023-10-07 12:31:10 -0500

    Install NextUI

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./src/**/*.{html,js}",
+    "./node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  darkMode: "class",
+  plugins: [nextui()],
+}
+
