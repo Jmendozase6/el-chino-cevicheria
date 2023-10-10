917a97bd50ea963ed346e076ec780d3a7345b3b5
Jhair Mendoza <Felinecode.dev@gmail.com>
2023-10-10 15:10:55 -0500

    remove: css import

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { NextUIProvider } from '@nextui-org/react'
-import App from './App'
-import './index.css'
+import { App } from './App'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
