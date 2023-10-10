a43124ffceec8a6023b3eecf1d273ed5f1ff5d56
Jhair Mendoza <Felinecode.dev@gmail.com>
2023-10-10 15:14:28 -0500

    add: menu section in dashboard

---
+++ src/components/home/menu-section.jsx
@@ -0,0 +1,58 @@
+import React from "react";
+import { Card, CardBody, CardFooter, Image } from "@nextui-org/react";
+
+export default function MenuSection() {
+  const list = [
+    {
+      title: "Orange",
+      img: "https://images.unsplash.com/photo-1555939594-58d7cb561ad1?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1287&q=80",
+      price: "$5.50",
+    },
+    {
+      title: "Tangerine",
+      img: "https://images.unsplash.com/photo-1555939594-58d7cb561ad1?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1287&q=80",
+      price: "$3.00",
+    },
+    {
+      title: "Raspberry",
+      img: "https://images.unsplash.com/photo-1555939594-58d7cb561ad1?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1287&q=80",
+      price: "$10.00",
+    },
+    {
+      title: "Lemon",
+      img: "https://images.unsplash.com/photo-1555939594-58d7cb561ad1?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1287&q=80",
+      price: "$5.30",
+    },
+
+  ];
+
+  return (
+    <div className='m-4 p-4 bg-white rounded-md shadow-md flex flex-col'>
+
+      <h2>Menú</h2>
+      <p>Selecciona tu comida favorita</p>
+      <div className="gap-2 grid grid-cols-2 sm:grid-cols-4">
+        {list.map((item, index) => (
+          <Card shadow="sm" key={index} isPressable
+            onPress={() => console.log("item pressed")}>
+            <CardBody className="overflow-visible p-0">
+              <Image
+                shadow="sm"
+                radius="lg"
+                width="100%"
+                isZoomed
+                alt={item.title}
+                className="w-full object-cover h-[140px]"
+                src={item.img}
+              />
+            </CardBody>
+            <CardFooter className="text-small justify-between">
+              <b>{item.title}</b>
+              <p className="text-default-400">{item.price}</p>
+            </CardFooter>
+          </Card>
+        ))}
+      </div>
+    </div>
+  );
+}
