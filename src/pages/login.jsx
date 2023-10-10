eda3120699f5642e8321799fc3e449da51f3593e
Jhair Mendoza <Felinecode.dev@gmail.com>
2023-10-10 15:15:09 -0500

    add: login page

---
+++ src/pages/login.jsx
@@ -0,0 +1,16 @@
+import { } from 'react'
+import PropTypes from 'prop-types'
+
+const Login = props => {
+	return (
+		<div>
+			Login
+		</div>
+	)
+}
+
+login.propTypes = {
+
+}
+
+export default login
